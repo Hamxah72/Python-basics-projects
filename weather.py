03c159d07693581903c1df4dad0b3f6b1279c78c
Hamxah72 <154810847+Hamxah72@users.noreply.github.com>
2024-01-16 20:56:42 +0100

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,28 @@
+import requests
+# import dotenv
+from dotenv import load_dotenv
+import os
+from pprint import pprint
+
+load_dotenv()
+
+def get_current_weather():
+    print("\n*** Get current Weather Conditions ***\n")
+
+    city = input("\nPlease enter a city name:\n")
+
+    request_url = f"https://api.openweathermap.org/data/3.0/onecall?appid={os.getenv('API_KEY')}&q={city}units=metric"
+
+    # print(request_url)
+
+    weather_data = requests.get(request_url).json()
+
+    # pprint(weather_data)
+
+    print(f"\nCurrent weather for {weather_data['name']}")
+    print(f"\nThe temp is{weather_data['name']['temp']}")
+    print(
+        f"\nFeels like {weather_data['name']['feels_like']} and{weather_data['weather'][0]['description']}.")
+
+if __name__ == "__main__":
+    get_current_weather()
