895b936fe74f3b3625de92bd2d7ad58f4af21fe7
Ishaq Hamza Zakariyya <154810847+Hamxah72@users.noreply.github.com>
2024-02-19 22:14:18 +0100

    Add files via upload

---
+++ 65_boxes.py
@@ -0,0 +1,39 @@
+from tkinter import *
+
+root = Tk()
+root.title("Entry Boxes")
+root.geometry("900x500")
+
+my_entries = []
+
+def something():
+    entry_list = ""
+
+    for entries in my_entries:
+        entry_list = entry_list + str(entries.get()) + "\n"
+        my_label.config(text=entry_list)
+
+    # print(my_entries[0].get())
+
+
+# Row Loop
+for y in range(5):
+    # Column Loop 
+    for x in range(5):
+        my_entry = Entry(root)
+        my_entry.grid(row=y, column=x, pady=20, padx=5)
+        my_entries.append(my_entry)
+
+
+my_button = Button(root, text="Click Me!", command=something)
+my_button.grid(row=6, column=2, pady=20)
+
+my_label = Label(root, text="")
+my_label.grid(row=7, column=2, pady=20)
+
+
+
+
+
+
+root.mainloop()
