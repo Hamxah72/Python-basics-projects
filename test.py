5522053dc45c62ad5c05f6868c409733ab6feae6
Ishaq Hamza Zakariyya <154810847+Hamxah72@users.noreply.github.com>
2024-03-05 23:24:29 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+# f = open("text.txt")
+# print(f.read())
+
+# f = open("text.txt", "w")
+# f.write("This will overwrite the content of our file")
+# f = open("text.txt")
+# print(f.read())
+
+# f = open("text.txt", "a")
+# f.write("\nThis text will be appended to the file")
+# f = open("text.txt")
+# print(f.read())
+
+f = open("text2.txt", "x")
